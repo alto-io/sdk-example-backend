8eae66346f85d6e74b791a42bb6cdc43dc5c9ba9
inn3rGames <hi@inn3r.com>
2022-02-14 23:56:12 +0200

    Add blocks

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -9,18 +9,61 @@ export class MyRoom extends Room<MyRoomState> {
     height: 800,
   };
 
-  playerGravity = 0.5;
+  playerGravity = 1;
   playerVelocityY = 0;
-  playerJumpForce = -10;
+  playerJumpForce = -15;
+
+  blocksWidth = 200;
+  blocksOffsetDistance = 200;
+  blocksOffset = this.setBlocksGroupOffset(
+    -this.blocksOffsetDistance,
+    this.blocksOffsetDistance
+  );
+  blocksVerticalDistance = 150;
+
+  score = 0;
 
   player = {
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
+    x: 400,
+    y: 400,
+    width: 160,
+    height: 160,
+  };
+
+  topBlock = {
+    x: 900,
+    y: -85,
+    height: 800,
+    width: 200,
+  };
+
+  bottomBlock = {
+    x: 900,
+    y: 1014,
+    height: 800,
+    width: 200,
+  };
+
+  invisibleScoreBlock = {
+    x: 400,
+    y: 400,
+    height: 800,
+    width: 200,
+    scored: false,
+  };
+
+  serverObjects = {
+    player: this.player,
+    topBlock: this.topBlock,
+    bottomBlock: this.bottomBlock,
+    invisibleScoreBlock: this.invisibleScoreBlock,
   };
 
   loop!: Delayed;
+
+  setBlocksGroupOffset(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
   updatePlayer() {
     this.playerVelocityY += this.playerGravity;
@@ -39,21 +82,48 @@ export class MyRoom extends Room<MyRoomState> {
     this.playerVelocityY = this.playerJumpForce;
   }
 
+  updateBlocks() {
+    this.topBlock.x -= 10;
+    if (this.topBlock.x < -this.topBlock.width * 0.5) {
+      this.blocksOffset = this.setBlocksGroupOffset(
+        -this.blocksOffsetDistance,
+        this.blocksOffsetDistance
+      );
+      this.topBlock.x = this.game.width + this.topBlock.width * 0.5;
+      this.topBlock.y =
+        this.game.height + this.blocksVerticalDistance - this.blocksOffset;
+      this.invisibleScoreBlock.scored = false;
+    }
+
+    if (this.invisibleScoreBlock.scored === false) {
+      this.invisibleScoreBlock.x =
+        this.topBlock.x + this.invisibleScoreBlock.width * 0.5;
+    }
+
+    this.bottomBlock.x -= 10;
+    if (this.bottomBlock.x < -this.bottomBlock.width * 0.5) {
+      this.bottomBlock.x = this.game.width + this.bottomBlock.width * 0.5;
+      this.bottomBlock.y = 0 - this.blocksVerticalDistance - this.blocksOffset;
+    }
+  }
+
   update = () => {
     //console.log(this);
     this.updatePlayer();
-    this.broadcast("update", this.player);
-  }
+    this.updateBlocks();
+
+    this.serverObjects = {
+      player: this.player,
+      topBlock: this.topBlock,
+      bottomBlock: this.bottomBlock,
+      invisibleScoreBlock: this.invisibleScoreBlock,
+    };
+
+    this.broadcast("update", this.serverObjects);
+  };
 
   onCreate(options: any) {
     this.setState(new MyRoomState());
-
-    this.player = {
-      x: 400,
-      y: 400,
-      width: 160,
-      height: 160,
-    };
 
     this.onMessage("jump", (client, message) => {
       this.makePlayerJump();
